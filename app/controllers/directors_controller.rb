250d39bf480f1c32912908a76a25f3953ef9f762
MichelleZ96 <michelle.zhang@ChicagoBooth.edu>
2024-11-06 23:12:28 +0000

    Adds youngest and eldest director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -3,6 +3,18 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/list"})
   end
 
+  def junior
+    director_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @the_youngest_director = director_records.at(-1)
+    render({ :template => "director_templates/young"})
+  end 
+
+  def senior
+    director_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @the_eldest_director = director_records.at(0)
+    render({ :template => "director_templates/senior"})
+  end 
+
   def show
     the_id = params.fetch("the_id")
 
